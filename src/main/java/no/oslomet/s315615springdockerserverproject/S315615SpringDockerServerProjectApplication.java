64f7137f540fcec7392919a358721d92557c63e2
vartdalen <viktor.v.johansen@hotmail.com>
2019-03-12 12:24:29 +0100

    hello world

---
+++ src/main/java/no/oslomet/s315615springdockerserverproject/S315615SpringDockerServerProjectApplication.java
@@ -0,0 +1,13 @@
+package no.oslomet.s315615springdockerserverproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S315615SpringDockerServerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S315615SpringDockerServerProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/no/oslomet/s315615springdockerserverproject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package no.oslomet.s315615springdockerserverproject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(S315615SpringDockerServerProjectApplication.class);
+    }
+
+}
